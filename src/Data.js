66c1061f48b68ca73febaeeabbaffd4f701ca986
LeoKing33 <104600694+LeoKing33@users.noreply.github.com>
2022-09-25 18:23:17 -0500

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+import './App.css';
+import { FcExpand, FcCollapse } from "react-icons/fc";
+
+const Data = ({title, body}) => {
+const [show, setShow] = useState(false);
+
+const iconClick = () => {
+    setShow(!show);
+};
+
+    return(
+        <div className={show ? 'accordion-opened accordion-item' : 'accordion-item'} onClick={iconClick}>
+            <div className='accordion-title'>
+                <h5>{title}</h5>
+                <p>{show ? <FcCollapse size={20}/> : <FcExpand size={20}/>}</p>
+            </div>
+            {show && <p>{body}</p>}
+        </div>        
+    )
+}
+
+export default Data;
